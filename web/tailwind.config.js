3984c3ef2aa6ccdb090acf5834be880cb483e777
FourLineCode <akmal3535.ah@gmail.com>
2021-09-07 19:13:30 +0600

    sidebar overflow bug fixed

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -16,5 +16,5 @@ module.exports = {
 	variants: {
 		extend: {},
 	},
-	plugins: [require('@tailwindcss/line-clamp')],
+	plugins: [require('@tailwindcss/line-clamp'), require('tailwind-scrollbar')],
 };
