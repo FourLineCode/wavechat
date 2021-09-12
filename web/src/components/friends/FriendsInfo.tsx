7a0bcba23a9dbe47b47f66ac72f51aac64a0569b
FourLineCode <akmal3535.ah@gmail.com>
2021-09-12 18:56:08 +0600

    friend request show + overflow bug still there

--- web/src/components/friends/FriendsInfo.tsx
+++ web/src/components/friends/FriendsInfo.tsx
@@ -1,5 +1,6 @@
 import { Tab } from '@headlessui/react';
 import clsx from 'clsx';
+import { Fragment } from 'react';
 import { FriendsList } from 'src/components/friends/FriendsList';
 import { Requestslist } from 'src/components/friends/RequestsList';
 
@@ -15,30 +16,32 @@ const tabs = [
 
 export function FriendsInfo() {
 	return (
-		<div className='w-64 h-full p-2 xl:w-80 bg-dark-800'>
-			<Tab.Group>
-				<Tab.List className='flex w-full overflow-hidden rounded-lg'>
-					{tabs.map((tab) => (
-						<Tab
-							className={({ selected }) =>
-								clsx(
-									'flex-1 p-1.5 flex justify-center items-center bg-dark-700 transition focus:outline-none font-semibold text-lg',
-									selected
-										? 'bg-brand-500'
-										: 'hover:bg-brand-300 hover:bg-opacity-30'
-								)
-							}
-							key={tab.id}
-						>
-							{tab.title}
-						</Tab>
-					))}
-				</Tab.List>
-				<Tab.Panels>
-					<Tab.Panel>
+		<div className='w-64 h-full overflow-y-auto scrollbar-none xl:w-80 bg-dark-800'>
+			<Tab.Group as='div' className='flex flex-col'>
+				<div className='sticky top-0 p-2 pb-3 bg-dark-800'>
+					<Tab.List className='flex w-full overflow-hidden rounded-lg'>
+						{tabs.map((tab) => (
+							<Tab
+								className={({ selected }) =>
+									clsx(
+										'flex-1 p-1.5 flex justify-center items-center bg-dark-700 transition focus:outline-none font-semibold text-lg',
+										selected
+											? 'bg-brand-500'
+											: 'hover:bg-brand-300 hover:bg-opacity-30'
+									)
+								}
+								key={tab.id}
+							>
+								{tab.title}
+							</Tab>
+						))}
+					</Tab.List>
+				</div>
+				<Tab.Panels className='flex-1 px-2 pb-2'>
+					<Tab.Panel as={Fragment}>
 						<FriendsList />
 					</Tab.Panel>
-					<Tab.Panel>
+					<Tab.Panel as={Fragment}>
 						<Requestslist />
 					</Tab.Panel>
 				</Tab.Panels>
--- web/src/components/friends/RequestsList.tsx
+++ web/src/components/friends/RequestsList.tsx
@@ -1,6 +1,7 @@
 import { gql, useQuery } from '@apollo/client';
 import { BarLoader } from 'react-spinners';
-import { PendingRequestsQuery } from 'src/apollo/__generated__/types';
+import { FriendRequest, PendingRequestsQuery } from 'src/apollo/__generated__/types';
+import { FriendRequestCard } from 'src/components/friends/FriendRequestCard';
 
 const GET_PENDING_REQUESTS = gql`
 	query PendingRequests {
@@ -23,13 +24,15 @@ const GET_PENDING_REQUESTS = gql`
 export function Requestslist() {
 	const { data, loading } = useQuery<PendingRequestsQuery>(GET_PENDING_REQUESTS);
 
-	return (
-		<div>
-			{loading ? (
-				<BarLoader color='white' />
-			) : (
-				data?.pendingRequests.map((request) => <div>{request.fromUser.displayName}</div>)
-			)}
+	return loading ? (
+		<div className='flex justify-center h-full pt-16'>
+			<BarLoader color='silver' speedMultiplier={1.5} />
+		</div>
+	) : (
+		<div className='space-y-2'>
+			{data?.pendingRequests.map((request) => (
+				<FriendRequestCard request={request as FriendRequest} />
+			))}
 		</div>
 	);
 }
