324ad312512ef18e742fd2477841af1db17d5466
FourLineCode <akmal3535.ah@gmail.com>
2021-10-18 16:29:27 +0600

    dev script build flag

--- dev.sh
+++ dev.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
 docker-compose down --volumes
-docker-compose up --no-start
+if [ "$1" == "build" ]; then
+	docker-compose up --build --no-start --remove-orphans
+else
+	docker-compose up --no-start --remove-orphans
+fi
 
 docker-compose start postgres
 pnpm --dir api db:reset
 docker-compose stop postgres
 
-docker-compose up --remove-orphans
+docker-compose up
