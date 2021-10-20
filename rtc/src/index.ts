40cc36c0dd4ee1c06bca3f992682ea9d5ab40514
FourLineCode <akmal3535.ah@gmail.com>
2021-10-20 17:58:32 +0600

    shared types + path alias added

--- rtc/src/index.ts
+++ rtc/src/index.ts
@@ -1,3 +1,4 @@
+import { User } from '@shared/types/User';
 import http from 'http';
 import os from 'os';
 import { config } from 'src/internal/config';
@@ -5,6 +6,10 @@ import { config } from 'src/internal/config';
 http.createServer((_req, res) => {
 	res.write('Hello World!\n');
 	res.write(`From Server: ${os.hostname()}\n`);
+
+	const user: User = { id: 1, name: 'Akmal', age: 21 };
+	res.write(`${JSON.stringify(user, null, 2)}\n`);
+
 	res.end();
 }).listen(config.port);
 console.log(`\nServer is now running on http://localhost:${config.port}\n`);
